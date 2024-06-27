21ef0cbc6d92de150cd35dc8a9a629eff20b7535
Younsi wassim <120186938+wassimMin@users.noreply.github.com>
2024-06-27 15:45:38 +0100

    pp

--- app/src/main/java/com/example/trackwokrout/DayActivity.java
+++ app/src/main/java/com/example/trackwokrout/DayActivity.java
@@ -1,14 +1,47 @@
 package com.example.trackwokrout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class DayActivity extends AppCompatActivity {
+    private LinearLayout exerciseContainer;
+    Button btnAddExercise = findViewById(R.id.btn_add_exercise);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_day);
+        exerciseContainer = findViewById(R.id.exercise_container);
+        btnAddExercise = findViewById(R.id.btn_add_exercise);
+        btnAddExercise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addExerciseSection();
+            }
+        });
+    }
+    private void addExerciseSection(){
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View exerciseSection = inflater.inflate(R.layout.exercise_input_section, exerciseContainer,false);
+        EditText set_sets = exerciseSection.findViewById(R.id.set_sets);
+        LinearLayout setContainer = exerciseSection.findViewById(R.id.set_container);
+        set_sets.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus){
+
+                }
+            }
+        });
+    }
+    private void addSetInput(){
+
     }
 }
--- app/src/main/java/com/example/trackwokrout/days.java
+++ app/src/main/java/com/example/trackwokrout/days.java
@@ -1,14 +1,29 @@
 package com.example.trackwokrout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class days extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class days extends AppCompatActivity {
+    private RecyclerView recyclerViewDays;
+    private DayAdapter dayAdapter;
+    private List<Day> dayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_days);
+        recyclerViewDays = findViewById(R.id.recyclerViewDays);
+        recyclerViewDays.setLayoutManager(new LinearLayoutManager(this));
+        dayList = new ArrayList<>();
+        for(int i=1;i<=7;i++){
+            dayList.add(new Day("Day "+ i));
+        }
+        dayAdapter = new DayAdapter(dayList,this);
+        recyclerViewDays.setAdapter(dayAdapter);
     }
 }
